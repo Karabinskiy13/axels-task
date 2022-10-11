1a805039632326250c1b57a3d43ddcc85c3c6ce0
Karabinskiy13 <ivan.karabinskiy1308@gmail.com>
2022-10-11 11:23:14 +0300

    fix test

--- src/__tests__/PictureList.tsx
+++ src/__tests__/PictureList.tsx
@@ -8,7 +8,20 @@ import { QueryParamProvider } from 'use-query-params';
 import store from '../redux/store';
 import { PictureList } from '../components';
 
-test('Should render App component', async () => {
+test('Should render Header component', async () => {
+  render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <QueryParamProvider adapter={ReactRouter6Adapter}>
+          <PictureList />
+        </QueryParamProvider>
+      </BrowserRouter>
+    </Provider>
+  );
+  await screen.findByText('Picture Application');
+  expect(screen.getByText('Picture Application')).toHaveTextContent('Picture Application');
+});
+test('Should create snapshot', async () => {
   const { asFragment } = render(
     <Provider store={store}>
       <BrowserRouter>
@@ -18,7 +31,5 @@ test('Should render App component', async () => {
       </BrowserRouter>
     </Provider>
   );
-  await screen.findByPlaceholderText('Press enter to add new tag');
-  expect(screen.getByPlaceholderText('Press enter to add new tag'));
   expect(asFragment()).toMatchSnapshot();
 });
